fbc88172f895612d96c5134ed3ccffc420bba58f
ふくはら＠プログラミング勉強中 <70099238+fuku100@users.noreply.github.com>
2020-10-21 18:53:55 +0900

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,149 @@
+'use strict';
+
+{
+  const open1 = document.getElementById('open1');
+  const close1 = document.getElementById('close1');
+  const modal1 = document.getElementById('modal1');
+  const mask = document.getElementById('mask');
+
+  open1.addEventListener('click',() => {
+    modal1.classList.remove('hidden');
+    mask.classList.remove('hidden');
+  });
+  close1.addEventListener('click',() => {
+    modal1.classList.add('hidden');
+    mask.classList.add('hidden');
+  });
+  mask.addEventListener('click',() => {
+    close1.click();
+  });
+
+  const open2 = document.getElementById('open2');
+  const close2 = document.getElementById('close2');
+  const modal2 = document.getElementById('modal2');
+
+  open2.addEventListener('click',() => {
+    modal2.classList.remove('hidden');
+    mask.classList.remove('hidden');
+  });
+  close2.addEventListener('click',() => {
+    modal2.classList.add('hidden');
+    mask.classList.add('hidden');
+  });
+  mask.addEventListener('click',() => {
+    close2.click();
+  });
+
+  const open3 = document.getElementById('open3');
+  const close3 = document.getElementById('close3');
+  const modal3 = document.getElementById('modal3');
+
+  open3.addEventListener('click',() => {
+    modal3.classList.remove('hidden');
+    mask.classList.remove('hidden');
+  });
+  close3.addEventListener('click',() => {
+    modal3.classList.add('hidden');
+    mask.classList.add('hidden');
+  });
+  mask.addEventListener('click',() => {
+    close3.click();
+  });
+
+  const open4 = document.getElementById('open4');
+  const close4 = document.getElementById('close4');
+  const modal4 = document.getElementById('modal4');
+
+  open4.addEventListener('click',() => {
+    modal4.classList.remove('hidden');
+    mask.classList.remove('hidden');
+  });
+  close4.addEventListener('click',() => {
+    modal4.classList.add('hidden');
+    mask.classList.add('hidden');
+  });
+  mask.addEventListener('click',() => {
+    close4.click();
+  });
+
+  const open5 = document.getElementById('open5');
+  const close5 = document.getElementById('close5');
+  const modal5 = document.getElementById('modal5');
+
+  open5.addEventListener('click',() => {
+    modal5.classList.remove('hidden');
+    mask.classList.remove('hidden');
+  });
+  close5.addEventListener('click',() => {
+    modal5.classList.add('hidden');
+    mask.classList.add('hidden');
+  });
+  mask.addEventListener('click',() => {
+    close5.click();
+  });
+
+  const open6 = document.getElementById('open6');
+  const close6 = document.getElementById('close6');
+  const modal6 = document.getElementById('modal6');
+
+  open6.addEventListener('click',() => {
+    modal6.classList.remove('hidden');
+    mask.classList.remove('hidden');
+  });
+  close6.addEventListener('click',() => {
+    modal6.classList.add('hidden');
+    mask.classList.add('hidden');
+  });
+  mask.addEventListener('click',() => {
+    close6.click();
+  });
+
+  const open7 = document.getElementById('open7');
+  const close7 = document.getElementById('close7');
+  const modal7 = document.getElementById('modal7');
+
+  open7.addEventListener('click',() => {
+    modal7.classList.remove('hidden');
+    mask.classList.remove('hidden');
+  });
+  close7.addEventListener('click',() => {
+    modal7.classList.add('hidden');
+    mask.classList.add('hidden');
+  });
+  mask.addEventListener('click',() => {
+    close7.click();
+  });
+
+  const open8 = document.getElementById('open8');
+  const close8 = document.getElementById('close8');
+  const modal8 = document.getElementById('modal8');
+
+  open8.addEventListener('click',() => {
+    modal8.classList.remove('hidden');
+    mask.classList.remove('hidden');
+  });
+  close8.addEventListener('click',() => {
+    modal8.classList.add('hidden');
+    mask.classList.add('hidden');
+  });
+  mask.addEventListener('click',() => {
+    close8.click();
+  });
+
+  const open9 = document.getElementById('open9');
+  const close9 = document.getElementById('close9');
+  const modal9 = document.getElementById('modal9');
+
+  open9.addEventListener('click',() => {
+    modal9.classList.remove('hidden');
+    mask.classList.remove('hidden');
+  });
+  close9.addEventListener('click',() => {
+    modal9.classList.add('hidden');
+    mask.classList.add('hidden');
+  });
+  mask.addEventListener('click',() => {
+    close9.click();
+  });
+
+}
